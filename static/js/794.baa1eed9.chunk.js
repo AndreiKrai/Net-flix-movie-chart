5e6c3bd7226d54f0468f363f24c7c6d22318d9f2
AndreiKrai <107999344+AndreiKrai@users.noreply.github.com>
2023-01-01 18:26:51 +0000

    Deploying to gh-pages from @ AndreiKrai/Net-flix-movie-chart@d06a4f41ac361179555dcd54b0c3f83be6fd70d0 🚀

---
+++ static/js/794.baa1eed9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[794],{1794:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});n(2791);var r=n(184);function u(){return(0,r.jsx)("h1",{children:"NotFoundPage"})}}}]);
+//# sourceMappingURL=794.baa1eed9.chunk.js.map
