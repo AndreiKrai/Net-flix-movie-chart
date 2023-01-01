9ec023e34d4c9a09473a59230295380ef17668bf
AndreiKrai <okraichenko.a@gmail.com>
2023-01-01 20:08:24 +0200

    trailerMovie

--- src/components/ActorsSlider/ActorsSlider.jsx
+++ src/components/ActorsSlider/ActorsSlider.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
+import '../../../node_modules/slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { Link } from 'react-router-dom';
 import instance from 'helpers/axios';
